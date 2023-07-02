9a05e32ed4ad9ee0ccae5382b47ad8a456d0c841
ChavanAbhishek11 <chavanabhishek724@gmail.com>
2023-07-02 23:11:21 +0530

    first commit

---
+++ src/pages/AboutUsPage.js
@@ -0,0 +1,90 @@
+import "./AboutUsPage.css";
+const AboutUsPage = () => {
+  return (
+    <div className="about-us-page">
+      <div className="about-us-page-child" />
+      <div className="about-us-page-item" />
+      <div className="about-us-page-inner" />
+      <div className="ellipse-div" />
+      <div className="rectangle-div" />
+      <img className="rectangle-icon" alt="" src="/rectangle-2659@2x.png" />
+      <img
+        className="about-us-page-child1"
+        alt=""
+        src="/rectangle-2653@2x.png"
+      />
+      <img
+        className="about-us-page-child2"
+        alt=""
+        src="/rectangle-2654@2x.png"
+      />
+      <img
+        className="about-us-page-child3"
+        alt=""
+        src="/rectangle-2656@2x.png"
+      />
+      <div className="about-us-page-child4" />
+      <img
+        className="about-us-page-child5"
+        alt=""
+        src="/rectangle-2657@2x.png"
+      />
+      <img
+        className="about-us-page-child6"
+        alt=""
+        src="/rectangle-2655@2x.png"
+      />
+      <b className="driffle-is-a-container">
+        <span>{`Driffle is a `}</span>
+        <span className="one-stop-shop">one-stop shop</span>
+        <span> for all your gaming needs.</span>
+      </b>
+      <div className="not-only-do-container">
+        <p className="not-only-do">{`Not only do we sell games, but also offer gift cards, DLC's, and more. Driffle is a marketplace, so you can find a wide variety of items from different sellers in one place. We are committed to the future of gaming and strive to make our platform safe, affordable, and sustainable for all users. Whether you're looking for the latest releases `}</p>
+        <p className="not-only-do">
+          or hard-to-find classics, you're sure to find what you're looking for
+          on Driffle.
+        </p>
+      </div>
+      <div className="component-34">
+        <div className="component-34-child" />
+        <img className="arrow-back-24-icon" alt="" src="/arrowback24.svg" />
+        <b className="visit-our-store">Visit our store</b>
+      </div>
+      <div className="rectangle-parent">
+        <div className="group-child" />
+        <img
+          className="logo-horizontal-white-no"
+          alt=""
+          src="/logo--horizontal--white--no-padding.svg"
+        />
+        <div className="ui-cart-notification">
+          <img
+            className="ui-cart-notification-child"
+            alt=""
+            src="/group-184.svg"
+          />
+          <div className="ui-cart-notification-item" />
+          <b className="b">2</b>
+        </div>
+        <img
+          className="ui-favorite-default"
+          alt=""
+          src="/ui--favoritedefault.svg"
+        />
+        <div className="ui-region">
+          <div className="region-24-parent">
+            <img className="region-24-icon" alt="" src="/region24.svg" />
+            <div className="px-spacing" />
+            <b className="india-english">India (English)</b>
+            <div className="frame-child" />
+            <b className="india-english">INR</b>
+          </div>
+        </div>
+        <img className="profile-24-icon" alt="" src="/profile24.svg" />
+      </div>
+    </div>
+  );
+};
+
+export default AboutUsPage;
